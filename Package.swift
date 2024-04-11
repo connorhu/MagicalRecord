86a3427908d872e5ec3e769c4eeef336879409f3
connor <connor@connor.hu>
2024-04-11 10:54:56 +0200

    downgrade downgraded swift tools version to 5.5 downgrade lumberjack version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", exact: "3.8.2"),
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack",
+                 .upToNextMajor(from: "3.7.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
